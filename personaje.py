c9a68d1a00bcbc6152818a2cba5340d03024d0cc
EdgarEscobedoZ <125921549+EdgarEscobedoZ@users.noreply.github.com>
2023-02-24 14:53:57 -0600

    Encapsulamiento. Creación de get set

--- personaje.py
+++ personaje.py
@@ -3,21 +3,41 @@ class Personaje:
     # agregamos el constructtor del personaje
     def __init__ (self, esp, nom, alt):
         #Atributos
-        self.especie = esp
-        self.nombre = nom
-        self.altura = alt
+        self.__especie = esp
+        self.__nombre = nom
+        self.__altura = alt
     
     #metodos
     def correr(self, status):
         if (status):
-            print("El personaje "+ self.nombre + " está corriendo")
+            print("El personaje "+ self.__nombre + " está corriendo")
         else:
-            print("El personaje "+ self.nombre + " se detuvo")            
+            print("El personaje "+ self.__nombre + " se detuvo")            
             
     def lanzarGranadas(self):
-        print("El personaje " + self.nombre +' lanzó la granada')
+        print("El personaje " + self.__nombre +' lanzó la granada')
         
     def recargarArma(self,municion):
         cargador=10
         cargador=cargador + municion
         print('El arma recargada tiene '+str(cargador)+" balas")
+        
+    #Gets y sets 
+    
+    def getEspecie(self):
+        return self.__especie
+    def setEspecie(self, esp):
+        self.__especie=esp
+        
+    def getNombre(self):
+        return self.__nombre
+    def setNombre(self, nom):
+        self.__nombre=nom
+    
+    def getAltura(self):
+        return self.__altura
+    def setAltura(self, alt):
+        self.__altura=alt
+    
+    def __pensar(self):
+        print('Estoy pensando........ ')
