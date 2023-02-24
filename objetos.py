fc74459201d94ebf88c56429e3d645527b3b0a13
EdgarEscobedoZ <125921549+EdgarEscobedoZ@users.noreply.github.com>
2023-02-24 14:53:14 -0600

    Agregar Get y un Set

--- objetos.py
+++ objetos.py
@@ -19,13 +19,13 @@ recargaV= int(input('Ingresa las balas para el Villano '))
 Heroe=Personaje(especieH, nombreH, alturaH)
 Villano=Personaje(especieV, nombreV, alturaV)
 
-
+Heroe.setNombre('Pepe Pecas')
 #3. Usamos los metodos y atributos del Heroe
 print('')
 print('### Atributos del Heroe ###')
-print('El personaje se llama '+ Heroe.nombre)
-print('Pertenece a la especie '+Heroe.especie)
-print('y una altura de '+ str(Heroe.altura))
+print('El personaje se llama '+ Heroe.getNombre())
+print('Pertenece a la especie '+Heroe.getEspecie())
+print('y una altura de '+ str(Heroe.getAltura()))
 
 Heroe.correr(True)
 Heroe.lanzarGranadas()
@@ -34,11 +34,12 @@ Heroe.recargarArma(recargaH)
 #4. Usar los metodos y atributos del villano
 print('')
 print('### Atributos del Villano ###')
-print('El personaje se llama '+ Villano.nombre)
-print('Pertenece a la especie '+Villano.especie)
-print('y una altura de '+ str(Villano.altura))
+print('El personaje se llama '+ Villano.getNombre())
+print('Pertenece a la especie '+Villano.getEspecie())
+print('y una altura de '+ str(Villano.getAltura()))
 
 Villano.correr(True)
 Villano.lanzarGranadas()
 Villano.recargarArma(recargaV)
+#Villano.__pensar()
 
