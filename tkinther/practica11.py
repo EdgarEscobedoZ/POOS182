433ee5005b6acce784029da6b0abfc04a6477e18
EdgarEscobedoZ <125921549+EdgarEscobedoZ@users.noreply.github.com>
2023-03-01 15:00:55 -0600

    Funcionalidades botones, Mensajes

--- tkinther/practica11.py
+++ tkinther/practica11.py
@@ -1,4 +1,19 @@
-from tkinter import Tk, Button, Frame
+from tkinter import Tk, Button, Frame, messagebox
+
+#5. agregar funcion de mensaje
+def mostrarMensaje():
+    messagebox.showinfo("Información", "Te informo que todo fallo con exito")
+    messagebox.showerror('Error','Perdon te falle!!')
+    print(messagebox.askokcancel('Pregunta', '¿Seguro que quieres guardar algo?'))
+    #print(messagebox.askquestion('Pregunta',"probando"))
+    #print(messagebox.askretrycancel('Pregunta',"probando"))
+    #print(messagebox.askyesno('Pregunta',"probando"))
+
+#6. Funcion agregar botones
+def agregarBoton():
+    botonVerde.config(text="+", bg="green", fg="white")
+    botonNuevo= Button(seccion3, text="Boton Nuevo")
+    botonNuevo.pack()
 
 #1. Generar una ventana
 ventana = Tk()
@@ -17,16 +32,19 @@ seccion3 = Frame(ventana, bg="purple")
 seccion3.pack(expand=True, fill='both')
 
 #3. Agregamos botones 
-botonAzul= Button(seccion1, text="boton azul", fg="blue")
+botonAzul= Button(seccion1, text="boton azul", fg="blue", bg="#73a9fa", command=mostrarMensaje)
 botonAzul.place(x=60,y=60, width= 100, height= 60)
 
-botonAmarillo= Button(seccion2, text="boton Amarillo", bg="yellow", fg="red")
+botonAmarillo= Button(seccion2, text="boton Amarillo", bg="#f4fa73", fg="red")
 botonAmarillo.grid(row=1, column=1)
 
-botonNegro= Button(seccion2, text="boton Negro", bg="black", fg="White")
+botonNegro= Button(seccion2, text="boton Negro", bg="#1a1b16", fg="White")
 botonNegro.grid(row = 0, column =0)
 
-botonVerde= Button(seccion3, text="boton Verde", bg="green", fg="White")
+botonVerde= Button(seccion3, text="boton Verde", bg="#74fa87", fg="White", command=agregarBoton)
 botonVerde.pack()
 
+#4. Posicionamiento de los Widgets
+
+#Metodo Main para la ejecucion de la ventana
 ventana.mainloop()
