cb74a54b4575a0eee1c88910572dea8983063089
EdgarEscobedoZ <125921549+EdgarEscobedoZ@users.noreply.github.com>
2023-03-09 15:26:09 -0600

    Creacion de seguridad(). Finalizar código

--- tkinther/validar.py
+++ tkinther/validar.py
@@ -10,43 +10,69 @@ class validacion:
     def validar(self,user, contra):
         return user == self.__usuario and contra == self.__contraseña
     
+    
+    
 class generacion:
-    def __init__ (self,caract):
-        self.__caracteres = caract
+    def __init__ (self):
+        self.__caracteres = 8
+        self.__especial = False
+        self.__mayusculas = False
         
-    
-    
-    def generar(self, x):
+    def validarContra(self, a):
+        if a>8:
+            self.__caracteres=a
+        else:
+            self.__caracteres = 8
+        
+    def generar(self):
         minusculas = string.ascii_lowercase
         caracteres_esp = string.punctuation
         letras = string.ascii_letters
         contraseña = ''
-        esp = messagebox.askyesno('Caracteres especiales','¿Desea usar caracteres especiales en su contraseña?')
-        mayus = messagebox.askyesno('Mayusculas','¿Desea usar mayusuclas en su contraseña?')
-        if x=="" :
-            self.__caracteres=8
-        if esp==True and mayus == True:
+        self.__especial = messagebox.askyesno('Caracteres especiales','¿Desea usar caracteres especiales en su contraseña?')
+        self.__mayusculas = messagebox.askyesno('Mayusculas','¿Desea usar mayusuclas en su contraseña?')
+        
+        if self.__especial==True and self.__mayusculas == True:
             alfabeto = caracteres_esp + letras
             for i in range (self.__caracteres):
                 contraseña+=''.join(secrets.choice(alfabeto))
             return contraseña
-        if esp==False and mayus == True:
+        if self.__especial==False and self.__mayusculas == True:
             alfabeto = letras
             for i in range (self.__caracteres):
                 contraseña+=''.join(secrets.choice(alfabeto))
             return contraseña
-        if esp==True and mayus == False:
+        if self.__especial==True and self.__mayusculas == False:
             alfabeto = caracteres_esp + minusculas
             for i in range (self.__caracteres):
                 contraseña+=''.join(secrets.choice(alfabeto))
             return contraseña
-        if esp==False and mayus == False:
+        if self.__especial==False and self.__mayusculas == False:
             alfabeto = minusculas
             for i in range (self.__caracteres):
                 contraseña+=''.join(secrets.choice(alfabeto))
             return contraseña
         
-        
-        
-    #def validarContra():
-        
+    def seguridad(self):
+        if self.__caracteres>=10:
+            if self.__especial==True:
+                if self.__mayusculas == True:
+                    messagebox.showinfo('Información', 'La contraseña es muy segura')
+                else:
+                    messagebox.showinfo('Información', 'La contraseña es segura')
+            if self.__especial==False:
+                if self.__mayusculas == True:
+                    messagebox.showinfo('Información', 'La contraseña es segura')
+                else:
+                    messagebox.showinfo('Información', 'La contraseña es medianamente segura')
+        if self.__caracteres<10:
+            if self.__especial==True:
+                if self.__mayusculas == True:
+                    messagebox.showinfo('Información', 'La contraseña es segura')
+                else:
+                    messagebox.showinfo('Información', 'La contraseña es medianamente segura')
+            if self.__especial==False:
+                if self.__mayusculas == True:
+                    messagebox.showinfo('Información', 'La contraseña medianamente segura')
+                else:
+                    messagebox.showinfo('Información', 'La contraseña no es muy segura')
