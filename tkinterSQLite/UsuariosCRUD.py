ca7d5bd229bcd50b4d8fa4eccb60adc778da2281
EdgarEscobedoZ <125921549+EdgarEscobedoZ@users.noreply.github.com>
2023-04-04 18:36:12 -0600

    Creacion pestana4

--- tkinterSQLite/UsuariosCRUD.py
+++ tkinterSQLite/UsuariosCRUD.py
@@ -11,6 +11,7 @@ controlador = controladorBD()
 def ejectutaInsert():
     controlador.guardarUsuario(varNom.get(), varCor.get(), varContra.get())
 #Funcion que usa mi objeto controlador para buscar 1 usuario
+
 def ejecutarSelectU():
     rsUsu = controlador.buscarUsuario(varBus.get())
     textBus.delete('1.0', END)
@@ -22,15 +23,40 @@ def ejecutarSelectU():
         textBus.insert(INSERT, cadena)
     else:
         messagebox.showinfo('No encontrado', 'Usuario no existe en BD')
+
+def ejecutarSelectU2():
+    rsUsuario = controlador.buscarUsuario(varElim.get())
+    tree2.delete(*tree2.get_children())
+
+    #Iteramos el contenido de la consulta y lo guardamos en CADENA 
+    for usu in rsUsuario:
+        cadena = tree2.insert('',tk.END, values=usu)
+    if (rsUsuario):
+        textBus.insert(INSERT, cadena)
+    else:
+        messagebox.showinfo('No encontrado', 'Usuario no existe en BD')
+        
 def ejecutarConsul():
     rsConsul = controlador.consultarUsuario()
     tree.delete(*tree.get_children())
     for fila in rsConsul:
         tree.insert('',tk.END, values=fila)
-        
-        
-        
-    
+
+def ejecutarModi():
+    rsConsul = controlador.consultarUsuario()
+    tree.delete(*tree.get_children())
+    for fila in rsConsul:
+        tree.insert('',tk.END, values=fila)
+
+def ejecutarEliminarU():
+    #Iteramos el contenido de la consulta y lo guardamos en CADENA 
+    ask = messagebox.askyesno('Pregunta', '¿Seguro que quiere eliminar el usuario?')
+    if ask == True:
+        controlador.eliminarUsuario(varElim.get())
+        tree2.delete(*tree2.get_children())
+        messagebox.showinfo('Info', 'Usuario Eliminado')
+    else:
+        messagebox.showerror('Error', 'Usuario no Eliminado')
 
 ventana = Tk()
 ventana.title('CRUD de usuarios')
@@ -75,27 +101,48 @@ textBus= tk.Text(pestana2, width=52, height=5)
 textBus.pack()
 
 #Pestaña 3: Consultar Usuario
+titulo3 = Label(pestana3, text='Consultar Usuarios', fg = 'purple', font=('Modern', 18)).pack()
 tree = ttk.Treeview(pestana3, column=("c1", "c2", "c3", 'c4'), show='headings')
 
 tree.column("#1", anchor=tk.CENTER)
-
 tree.heading("#1", text="ID")
 
 tree.column("#2", anchor=tk.CENTER)
-
 tree.heading("#2", text="Nombre")
 
 tree.column("#3", anchor=tk.CENTER)
-
 tree.heading("#3", text="Correo")
 
 tree.column("#4", anchor=tk.CENTER)
-
 tree.heading("#4", text="Contra")
 
 tree.pack()
 btnConsul= Button(pestana3,text='Consultar', command=ejecutarConsul).pack()
 
+#Pestaña 4
+titulo3 = Label(pestana4, text='Actualizar Usuarios', fg = 'black', font=('Modern', 18)).pack()
+
+varElim= tk.StringVar()
+lblP4=Label(pestana4,text='Identificador de Usuario:').pack()
+txtP4= Entry(pestana4,textvariable=varElim).pack()
+btnBusqueda= Button(pestana4,text='Buscar', command=ejecutarSelectU2).pack()
+btnElim= Button(pestana4,text='Eliminar', command=ejecutarEliminarU).pack()
+
+tree2 = ttk.Treeview(pestana4, column=("c1", "c2", "c3", 'c4'), show='headings')
+
+tree2.column("#1", anchor=tk.CENTER)
+tree2.heading("#1", text="ID")
+
+tree2.column("#2", anchor=tk.CENTER)
+tree2.heading("#2", text="Nombre")
+
+tree2.column("#3", anchor=tk.CENTER)
+tree2.heading("#3", text="Correo")
+
+tree2.column("#4", anchor=tk.CENTER)
+tree2.heading("#4", text="Contra")
+
+tree2.pack()
 
 panel.add(pestana1, text='Agregar usuarios')
 panel.add(pestana2, text='Buscar usuario')
